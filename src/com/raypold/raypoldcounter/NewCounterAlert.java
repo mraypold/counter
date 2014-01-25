cc0eb8edc5a7298c34c9925c9a7bf31b0b2fd3ff
mraypold <mraypold@gmail.com>
2014-01-25 15:21:26 -0700

    added .java with actionbar functionality

---
+++ src/com/raypold/raypoldcounter/NewCounterAlert.java
@@ -0,0 +1,56 @@
+package com.raypold.raypoldcounter;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class NewCounterAlert extends DialogFragment {
+	
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		LayoutInflater inflater = getActivity().getLayoutInflater();
+
+		builder.setView(inflater.inflate(R.layout.create_counter, null))
+				.setTitle(R.string.createCounter)
+				.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+							
+					@Override
+							public void onClick(DialogInterface dialog, int id) {
+
+								EditText textField = (EditText) getDialog().findViewById(R.id.createCounterTextField);
+								
+								String newCounterName = textField.getText().toString();
+
+								Counter newCounter = new Counter(newCounterName);
+								newCounter.saveCount();
+
+								Preferences userPreferences = new Preferences(MainActivity.context);
+								userPreferences.setLastOpenCounter(newCounterName);
+								
+								/* Important note:
+								 * 
+								 * Must call the fragments to refresh display from within this code, since Android will continue running
+								 * other threads while it waits for the user to confirm their action.
+								 */ 
+
+								CounterFragment.refreshDisplay();
+								Toast.makeText(MainActivity.context, R.string.counterCreated, Toast.LENGTH_SHORT).show();
+
+							}
+						})
+
+				.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								/* Do nothing */
+							}
+						});
+
+		return builder.create();
+	}
+}
